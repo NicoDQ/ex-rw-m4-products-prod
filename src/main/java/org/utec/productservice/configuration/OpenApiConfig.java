4c2d18f0f77f9a6b16a43b2d08e00d508818ca40
nicolas_duarte <nicolas.duarte@utec.edu.uy>
2025-06-13 11:45:02 -0300

    adds server config to OpenAPI

--- src/main/java/org/utec/productservice/configuration/OpenApiConfig.java
+++ src/main/java/org/utec/productservice/configuration/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,6 +19,9 @@ public class OpenApiConfig {
 
         return new OpenAPI()
                 .info(new Info().title("Products API").version("1.0"))
+                .servers(List.of(
+                        new Server().url("https://ex-rw-m4-prod-production.up.railway.app")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new io.swagger.v3.oas.models.Components()
